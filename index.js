994e672d16941a4a6c7b4482afcecf87f97bb770
Rutujadd27 <rutujadd27@gmail.com>
2023-02-20 13:42:24 +0530

    done

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const {connection} = require("./db")
+const {userRouter} = require("./routes/user.routes") 
+const {UserModel}  =require("./model/user.model")
+const {noteRouter} = require("./routes/Note.routes") 
+const {authonticate} = require("./middlewares/authonticate.middlewares")
+
+const cors=require("cors")
+
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res) => {
+    
+    res.send("Home-Page")
+})
+
+app.use("/users",userRouter)
+app.use(authonticate)
+
+app.use("/notes",noteRouter)
+
+app.get("/users",async(req,res) => {
+    let query = req.query
+    console.log(query)
+    
+    try{
+        const users = await UserModel.find(query)
+       res.send(users)
+  
+    }catch(err){ 
+        res.send({"msg":"cannot get the User","error":err.message})
+      
+    }
+    
+})
+
+app.listen(process.env.port, async() => {
+    try{
+        await connection
+
+        console.log("connected to the DB")
+    }
+    catch(err){
+        console.log(err)
+
+        console.log(" It is not connected to the DB")
+       
+    }
+    console.log(`Port runnig at ${process.env.port}`)
+   
+})
